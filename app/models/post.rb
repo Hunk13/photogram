ebacad0a5be183bf752080183016ccd55e5e6dd7
Aliaksandr Kalinka <alex.kalinoff@gmail.com>
2016-04-05 20:01:00 +0300

    add comment and spec for create and delete comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   validates :user_id, presence: true
   belongs_to :user
+  has_many :comments
   validates :image, presence: true
 
   has_attached_file :image, styles: { :medium => "640x" }
