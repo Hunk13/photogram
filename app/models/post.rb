2e19a62fe4550ca36d090dad2cee209b9934ee78
Alexandr Kalinka <alex.kalinoff@gmail.com>
2018-06-09 20:29:57 +0200

    add application_record.rb and change version of migrations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
-class Post < ActiveRecord::Base
+class Post < ApplicationRecord
   acts_as_votable
 
   belongs_to :user
--- db/migrate/20160415093339_add_default_value_to_notification_attribute.rb
+++ db/migrate/20160415093339_add_default_value_to_notification_attribute.rb
@@ -1,4 +1,4 @@
-class AddDefaultValueToNotificationAttribute < ActiveRecord::Migration
+class AddDefaultValueToNotificationAttribute < ActiveRecord::Migration[5.0]
   def change
     change_column_default :notifications, :read, from: nil, to: false
   end
